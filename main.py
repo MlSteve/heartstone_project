da4f562e5670a7bf887a63d26a068c942828d82f
MlSteve <ml.steve@mail.ru>
2023-02-06 22:00:58 +0300

    Added class Archer

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ class Mage:
         self.health = 100
         self.mana = 60
 
+class Archer:
+    def __init__(self):
+        self.health = 50
+        self.arrows_count = 20
+
 unit1 = Warrior()
 unit2 = Warrior()
 unit3 = Mage()
+unit4 = Archer()
 
 
 print(unit1.__dict__)
 print(unit2.__dict__)
 print(unit3.__dict__)
+print(unit4.__dict__)
