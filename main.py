cc2c2cc88d113fc7f136a532f0ec1b207ec8c5a7
MlSteve <ml.steve@mail.ru>
2023-02-06 21:13:56 +0300

    Added warrior and mage classes

---
+++ main.py
@@ -0,0 +1,18 @@
+class Warrior:
+    def __init__(self):
+        self.health = 100
+        self.stamina = 100
+
+class Mage:
+    def __init__(self):
+        self.health = 100
+        self.mana = 60
+
+unit1 = Warrior()
+unit2 = Warrior()
+unit3 = Mage()
+
+
+print(unit1.__dict__)
+print(unit2.__dict__)
+print(unit3.__dict__)
